411a8c03c5c7fd7f672f4b22ee1006a593b7dee3
Kidus Kinfe <53956331+KidusY@users.noreply.github.com>
2020-04-07 20:11:50 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react'
-import Nav from './Nav/Nav'
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import HomePage from './HomePage/HomePage'
-import './App.css'
+import React, { Component } from 'react';
+import Nav from './Nav/Nav';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import HomePage from './HomePage/HomePage';
+import MenuPage from './MenuPage/MenuPage';
+import Bio from './BioPage/BioPage';
+import './App.css';
+import NotFound from './NotFoundPage/NotFoundPage';
+import { Route, Switch } from 'react-router-dom';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <nav>
-          <Nav />
-        </nav>
-        <header>
-          <Header />
-        </header>
-        <main>
-          <HomePage />
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    )
-  }
+	render () {
+		return (
+			<div className='App'>
+				<nav>
+					<Nav />
+				</nav>
+				<header>
+					<Header />
+				</header>
+				<main>
+					<Switch>
+						<Route exact path='/' component={HomePage} />
+						<Route path='/menuPage' component={MenuPage} />
+						<Route exact path='/Bio' component={Bio} />
+						<Route component={NotFound} />
+					</Switch>
+				</main>
+				<footer>
+					<Footer />
+				</footer>
+			</div>
+		);
+	}
 }
